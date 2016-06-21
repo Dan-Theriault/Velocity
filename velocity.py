5e03d164fb2081041eebcb9818b2393e68b427ea
Daniel Theriault <dannymt97@gmail.com>
2016-06-21 12:11:22 -0400

    Neglected to include velocity.py in last commit. Configuration now sourced from velocity.conf through velocityconf.py

--- velocity.py
+++ velocity.py
@@ -3,56 +3,59 @@
 from pathlib import Path
 import subprocess
 from shutil import copyfile
+from velocityconfig import VelocityConfig
+
+velocity_dir = '/home/dtheriault3/Documents/CODE/Velocity'
 
 
-def velocity(filename, config='default'):
+def velocity(filename, bookname='Notes', config='default'):
     """Velocity implements the logic for opening pages.
 
     filename -- string representation of file, no extension
     config -- configuration sourced from velocity.config
 
-    The config MUST specify a notebook path and an extension.
+    The config MUST specify a bookpath and an extension.
     The program will attempt to provide sane defaults for editor, editor_args,
-    and, if applicable, template.
+    and, if applicable, template, but makes no guarantees.
     """
-    path_config = get_path_config(config)
-    book = path_config['book']
-    ext = path_config['extension']
-    page = book/(filename+ext)
+    # Configuration Variables
+    source = VelocityConfig(velocity_dir+'/velocity.conf')
+    config = source.read(bookname)
 
-    editor_config = get_editor_config(config, page)
-    editor = editor_config['editor']
-    editor_args = editor_config['editor_args']
-    if 'template' in editor_config:
-        template = editor_config['template']
+    book = Path(config['bookpath'])
+    page = book/(filename+config['extension'])
+
+    editor = config['editor']
+    editor_args = config['args']
+    editor_args[-1] = editor_args[-1] + ' ' + (page.as_posix())
+
+    if 'template' in config:
+        template = config['template']
     else:
         template = None
 
+    # The Velocity logic!
     if page.exists():
-        subprocess.Popen(editor, *editor_args])
+        subprocess.Popen([editor, *editor_args])
     else:
         if template is not None and template.exists():
             copyfile(template.as_posix(), page.as_posix())
         else:
             newfile = open(page.as_posix(), 'a')
             newfile.close()
-        subprocess.Popen(editor, *editor_args])
+        subprocess.Popen([editor, *editor_args])
 
 
-def get_path_config(config):
-    """get_path_config reads the notebook and extension from the config."""
-    # TODO Properly sourcing a configuration
+def get_config(config):
+    """dummy method to provide an example configuration.
+
+    This method has been replaced with velocityconfig's read method.
+    It is retained only as a debugging fallback.
+    """
     config = {
-            'book': Path('/home/dtheriault3/Notes'),
-            'extension': '.md'
-            }
-    return config
-
-
-def get_editor_config(config, page):
-    """get_editor_config reads editor options from the config."""
-    config = {
-            'editor': 'gnome-terminal',
-            'editor_args': ['-e', 'vim ' + page.as_posix()],
-            }
+        'bookpath': '/home/dtheriault3/Notes',
+        'extension': '.md',
+        'editor': 'gnome-terminal',
+        'editor_args': ['-e', 'vim '],
+        }
     return config
