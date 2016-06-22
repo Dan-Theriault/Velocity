37c2c91f7504dc58b6a194bc627c9ee67fb5120b
dannymt97 <dannymt97@gmail.com>
2016-06-21 23:50:02 -0400

    Moved configuration files to ~/.velocity/, added full support for templates, added a global default editor setting, and fixed a nasty bug in which the file path would not be passed to the editor when no custom args are in use

--- velocity.py
+++ velocity.py
@@ -2,15 +2,16 @@
 
 """velocity.py implements the main logic of the program."""
 
-from pathlib import Path
 import subprocess
-from shutil import copyfile
+from os.path import expanduser
+from pathlib import Path
+from shutil import copy
 from velocityconfig import VelocityConfig
 
-velocity_dir = '/home/dtheriault3/Documents/CODE/Velocity'
+configdir = expanduser('~') + '/.velocity'
 
 
-def velocity(filename, bookname='Notes', config='default'):
+def velocity(filename, book='Notes'):
     """Velocity implements the logic for opening pages.
 
     filename -- string representation of file, no extension
@@ -21,18 +22,23 @@ def velocity(filename, bookname='Notes', config='default'):
     and, if applicable, template, but makes no guarantees.
     """
     # Configuration Variables
-    source = VelocityConfig(velocity_dir+'/velocity.conf')
-    config = source.read(bookname)
+    source = VelocityConfig(configdir+'/velocity.conf')
+    config = source.read(book)
 
-    book = Path(config['bookpath'])
-    page = book/(filename+config['extension'])
+    bookpath = Path(config['bookpath'])
+    page = bookpath/(filename+config['extension'])
 
     editor = config['editor']
     editor_args = config['args']
-    editor_args[-1] = editor_args[-1] + ' ' + (page.as_posix())
+    if len(editor_args) > 0:
+        editor_args[-1] = editor_args[-1] + ' ' + (page.as_posix())
+    else:
+        editor_args = [page.as_posix()]
 
     if 'template' in config:
-        template = config['template']
+        template = Path(configdir + '/Templates/' + config['template'])
+        if not template.exists() and template.is_file():
+            template = None
     else:
         template = None
 
@@ -40,15 +46,15 @@ def velocity(filename, bookname='Notes', config='default'):
     if page.exists():
         subprocess.Popen([editor, *editor_args])
     else:
-        if template is not None and template.exists():
-            copyfile(template.as_posix(), page.as_posix())
+        if template is not None:
+            copy(template.as_posix(), page.as_posix())
         else:
             newfile = open(page.as_posix(), 'a')
             newfile.close()
         subprocess.Popen([editor, *editor_args])
 
 
-def get_config(config):
+def get_config():
     """dummy method to provide an example configuration.
 
     This method has been replaced with velocityconfig's read method.
--- velocityconfig.py
+++ velocityconfig.py
@@ -20,13 +20,22 @@ class VelocityConfig:
         self.config = {
             'bookpath': self.parser[bookname]['bookpath'],
             'extension': self.parser[bookname]['extension'],
-            'editor': self.parser[bookname]['editor'],
             'args': []
         }
+
+        # Get editor for book if available: else, use global default
+        if 'editor' in self.parser[bookname]:
+            self.config['editor'] = self.parser[bookname]['editor']
+        else:
+            self.config['editor'] = self.parser['GLOBAL']['editor']
+
         i = 1
         while 'arg' + str(i) in self.parser[bookname]:
             self.config['args'].append(self.parser[bookname]['arg'+str(i)])
             i = i + 1
+
+        if 'template' in self.parser[bookname]:
+            self.config['template'] = self.parser[bookname]['template']
 
         return self.config
 
